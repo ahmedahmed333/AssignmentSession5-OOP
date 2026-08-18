aa95e57ae938ba7fc74a60e62d12ca92d3775847
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-08-18 12:54:04 +0300

    Question 02

--- AssignmentSession6-OOP/Program.cs
+++ AssignmentSession6-OOP/Program.cs
@@ -5,26 +5,31 @@
         static void Main(string[] args)
         {
             #region Question 01 
-            DeliveryAddress address1 = new DeliveryAddress();
-            address1.City = "Giza";
-            DeliveryAddress address2 = address1;
-            Console.WriteLine(address1.City);//Giza
-            Console.WriteLine(address2.City);//Giza
-            address1.City = "cairo";
-            Console.WriteLine(address1.City);//cairo
-            Console.WriteLine(address2.City);//Giza , because DeliveryAddress is struct (value type)
-                                             //, so address2 is an independent copy. 
-            Console.WriteLine("===========================");
-            Customer customer1 = new Customer();
-            customer1.Name = "ahmed";
-            Customer customer2 = customer1;
-            Console.WriteLine(customer1.Name);//ahmed
-            Console.WriteLine(customer2.Name);//ahmed
-            customer1.Name = "ali";
-            Console.WriteLine(customer1.Name);//ali
-            Console.WriteLine(customer2.Name);//ali ,  beacuse Customer is class (reference type) ,
-                                              //Modifying the object through one variable will affect the other so customer2 will also change to ali
+            //DeliveryAddress address1 = new DeliveryAddress();
+            //address1.City = "Giza";
+            //DeliveryAddress address2 = address1;
+            //Console.WriteLine(address1.City);//Giza
+            //Console.WriteLine(address2.City);//Giza
+            //address1.City = "cairo";
+            //Console.WriteLine(address1.City);//cairo
+            //Console.WriteLine(address2.City);//Giza , because DeliveryAddress is struct (value type)
+            //                                 //, so address2 is an independent copy. 
+            //Console.WriteLine("===========================");
+            //Customer customer1 = new Customer();
+            //customer1.Name = "ahmed";
+            //Customer customer2 = customer1;
+            //Console.WriteLine(customer1.Name);//ahmed
+            //Console.WriteLine(customer2.Name);//ahmed
+            //customer1.Name = "ali";
+            //Console.WriteLine(customer1.Name);//ali
+            //Console.WriteLine(customer2.Name);//ali ,  beacuse Customer is class (reference type) ,
+            //Modifying the object through one variable will affect the other so customer2 will also change to ali
             #endregion
+
+            #region Question 02
+
+            #endregion
+
         }
     }
 }
---
+++ AssignmentSession6-OOP/shipment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentSession6_OOP
+{
+    public struct Shipment
+    {
+
+        // Question 02 (a) : 
+        // 1- dirct data (do data hiding) : Fields are marked as public , breaking encapsulation by allowing external code to access and modify internal data directly.
+        //2- Lack of Data Validation : There is no control over assigned values.
+        //3- Loss of Flexibility : Futrue business rules ot calc cannot be added later without breaking existing code that relises on these fields.
+
+        // Question 02 (b) : 
+        //  private fields
+        private double weight;
+        private decimal deliveryFee;
+        // Flexibility with validation (Auto-Property allows future modification without breaking code)
+        public string Description { get; set; }
+        //Data Validation
+        public double Weight
+        {
+            get { return weight; }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine("Weight cannot be negative");
+                }
+            }
+        }
+        public decimal DeliveryFee
+        {
+
+            get { return deliveryFee; }
+            set
+            {
+                if (value >= 0)
+                {
+                    deliveryFee = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Delivery Fee cannot be negative.");
+                }
+            }
+        }
+    }
+}
